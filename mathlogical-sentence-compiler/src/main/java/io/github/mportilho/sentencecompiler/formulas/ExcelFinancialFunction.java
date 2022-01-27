4a3298a9f0aa96938d51cc02eed6b8baf784df94
Marcelo Portilho <marcelo.portilho@bancoamazonia.com.br>
2022-01-27 17:43:09 -0300

    Added MIT License

--- mathlogical-sentence-compiler/src/main/java/io/github/mportilho/sentencecompiler/formulas/ExcelFinancialFunction.java
+++ mathlogical-sentence-compiler/src/main/java/io/github/mportilho/sentencecompiler/formulas/ExcelFinancialFunction.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2022. Marcelo Silva Portilho
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
 package io.github.mportilho.sentencecompiler.formulas;
 
 import java.math.BigDecimal;
@@ -11,7 +35,9 @@ import static java.math.BigDecimal.*;
 /**
  * Emulated financial formulas from Microsoft Excel, using BigDecimal class instead of double
  * <p>
- * Based on Apache Poi implementation: https://github.com/apache/poi
+ * Based on Apache Poi implementation (https://github.com/apache/poi), adapted to use BigDecimal with MathContext
+ * and adapted formulas to consider different values for number of compounding periods, number of payments and
+ * total number of periods
  */
 public class ExcelFinancialFunction {
 

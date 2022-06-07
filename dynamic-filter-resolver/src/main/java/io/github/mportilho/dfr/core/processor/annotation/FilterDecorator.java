8be36a729d3c2fc2d3bd600f93caa46575be7345
Marcelo Portilho <mportilho@protonmail.com>
2022-06-07 00:57:25 -0300

    Added empty filterdata for convenience

--- dynamic-filter-resolver/src/main/java/io/github/mportilho/dfr/core/processor/annotation/FilterDecorator.java
+++ dynamic-filter-resolver/src/main/java/io/github/mportilho/dfr/core/processor/annotation/FilterDecorator.java
@@ -1,9 +1,17 @@
 package io.github.mportilho.dfr.core.processor.annotation;
 
 import io.github.mportilho.commons.converters.FormattedConversionService;
+import io.github.mportilho.dfr.core.operation.FilterData;
+import io.github.mportilho.dfr.core.operation.type.Decorated;
 import io.github.mportilho.dfr.core.processor.ConditionalStatement;
+
+import java.util.Collections;
 
 public interface FilterDecorator<T> {
 
+    FilterData EMPTY_FILTERDATA = new FilterData("", "", new String[]{""}, Object.class, Decorated.class, false,
+            false, Collections.emptyList(), "", Collections.emptyMap(), "");
+
     T decorate(T filter, ConditionalStatement conditionalStatement, FormattedConversionService formattedConversionService);
+
 }
